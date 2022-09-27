ec041ac4e37629f780a746c35aa8fc0a375b24e2
Abdelrhman-Abdelaziz <85349614+Abdelrhman-Abdelaziz@users.noreply.github.com>
2022-09-27 23:10:49 +0200

    Add Utility Project

---
+++ MagicVilla_Utillity/SD.cs
@@ -0,0 +1,11 @@
+﻿namespace MagicVilla_Utillity
+{
+    public enum ApiType
+    {
+        GET,POST,PUT,UPDATE,DELETE
+    }
+    public static class SD
+    {
+        public static string SessionToken = "JWTToken";
+    }
+}
